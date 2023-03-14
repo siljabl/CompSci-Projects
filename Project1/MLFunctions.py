014056aedc46d54a478ba1677b69b37880cdc65e
silja uio <s.b.lastad@mn.uio.no>
2023-03-14 19:24:11 +0100

    updating notebook

--- Project1/MLFunctions.py
+++ Project1/MLFunctions.py
@@ -1,10 +1,7 @@
 import numpy as np
 from numpy import linalg
-#from sklearn.linear_model import LinearRegression, Ridge, Lasso
-#from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import train_test_split
-#from sklearn.pipeline import make_pipeline
-#from sklearn.utils import resample
+from sklearn.utils import resample
 
 
 def DesignMatrix(x, y, order):
@@ -39,13 +36,38 @@ def OLS_matrix_inversion(X, data):
 
 
 
-def bias_variance(data, n_bootstraps=500, max_degree=10, test_size=0.2):
+
+def error_bias2_variance(X, data, degree, n_bootstraps=500, test_size=0.2):
     '''
-    Performing bias variance analysis and returning bias^2 and variance
+    Performing bias variance analysis and returning error, bias^2 and variance
+    '''
+
+    # splitting data and design matrix in test and train 
+    X_train, X_test, y_train, y_test = train_test_split(X, data, test_size=test_size)
+        
+    # empty array for computing expectation value
+    y_pred = np.zeros([n_bootstraps, y_test.shape[0], y_test.shape[1]])
+        
+    for i in range(n_bootstraps):
+        X_, y_ = resample(X_train, y_train)
+        beta = OLS_matrix_inversion(X_, y_)
+        y_pred[i] = X_test @ beta
+
+    error = np.mean( np.mean((y_test - y_pred)**2, axis=0, keepdims=True) )
+    bias2 = np.mean( (y_test - np.mean(y_pred, axis=0, keepdims=True))**2 )
+    variance = np.mean( np.var(y_pred, axis=0, keepdims=True) )
+            
+    return error, bias2, variance
+
+
+
+def bias_variance_tradeoff(data, n_bootstraps=500, max_degree=10, test_size=0.2):
+    '''
+    Bias-variance tradeoff while looping through model complexity
     '''
     error = np.zeros(max_degree)
     bias2 = np.zeros(max_degree)
-    variance = np.zeros(max_degree)
+    var = np.zeros(max_degree)
     
     arr0 = np.linspace(0, 1, data.shape[0], endpoint=False)
     arr1 = np.linspace(0, 1, data.shape[1], endpoint=False)
@@ -54,19 +76,6 @@ def bias_variance(data, n_bootstraps=500, max_degree=10, test_size=0.2):
         # defining design matrix
         X = DesignMatrix(arr0, arr1, deg)
         
-        # splitting data and design matrix in test and train 
-        X_train, X_test, y_train, y_test = train_test_split(X, data, test_size=test_size)
-        
-        # empty array for computing expectation value
-        y_pred = np.zeros([n_bootstraps, y_test.shape[0], y_test.shape[1]])
-        
-        for i in range(n_bootstraps):
-            X_, y_ = resample(X_train, y_train)
-            beta = OLS_matrix_inversion(X_, y_)
-            y_pred[i] = X_test @ beta
-
-        error[deg] = np.mean( np.mean((y_test - y_pred)**2, axis=0) )
-        bias2[deg] = np.mean( (y_test - np.mean(y_pred, axis=0))**2 )
-        variance[deg] = np.mean( np.var(y_pred, axis=0) )
+        error[deg], bias2[deg], var[deg] = error_bias2_variance(X, data, deg, n_bootstraps=n_bootstraps, test_size=test_size)
             
-    return error, bias2, variance
+    return error, bias2, var
