88c8c71563f4a8878c4802af7224e089181645fc
silja uio <s.b.lastad@mn.uio.no>
2023-05-23 21:52:14 +0200

    updated code, Ridge and Lasso

--- Project1/MLFunctions.py
+++ Project1/MLFunctions.py
@@ -1,8 +1,10 @@
 import numpy as np
 from numpy import linalg
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.utils import resample
+from sklearn.linear_model import Lasso
 
+from Statistics import MSE
 
 def DesignMatrix(x, y, order):
     '''
@@ -35,6 +37,7 @@ def OLS_matrix_inversion(X, data):
     return beta
 
 
+
 def OLS_SVD(X, data):
     '''
     Performing ordinary least squares by singular matrix decomposition
@@ -50,9 +53,36 @@ def OLS_SVD(X, data):
 
 
 
-def error_bias2_variance(X, data, degree, n_bootstraps=500, test_size=0.2):
+def ridge_regression(X, data, lmbd):
     '''
-    Performing bias variance analysis and returning error, bias^2 and variance
+    Performing Ridge regression with matrix inversion
+    '''
+    XTX = (X.T @ X)
+    XTX += lmbd * np.eye(len(XTX))   # Ridge
+    beta = linalg.inv(XTX) @ X.T @ data
+    
+    return beta
+
+
+
+def lasso_regression(X, data, lmbd):
+    '''
+    Performing Lasso regression with matrix inversion
+
+    '''
+    LassoReg = Lasso(lmbd)
+    LassoReg.fit(X,data)
+    y_pred = LassoReg.predict(X)
+    
+    beta = LassoReg.coef_
+    
+    return beta, y_pred
+
+
+
+def bootstrap(X, data, n_bootstraps=500, test_size=0.2):
+    '''
+    Performing bootstrapping, returning error, bias^2 and variance
     '''
 
     # splitting data and design matrix in test and train sets
@@ -78,7 +108,42 @@ def error_bias2_variance(X, data, degree, n_bootstraps=500, test_size=0.2):
 
 
 
-def bias_variance_tradeoff(data, design_arr, n_bootstraps=500, max_degree=10, test_size=0.2):
+def cross_validation(X, data, test_size=0.2):
+    '''
+    Performing cross validation, returning error, bias^2 and variance
+    '''
+
+    # splitting data and design matrix in test and train sets
+    n_splits = int(1 / test_size)
+    kf = KFold(n_splits=n_splits, shuffle=True, random_state=None)  
+
+    #y_pred = np.zeros([n_splits, y_test.shape[0], y_test.shape[1]])
+    y_test = []
+    y_pred = []
+    for train_index, test_index in kf.split(X):
+        X_train, y_train = X[train_index], data[train_index]
+        X_test = X[test_index]
+
+        beta = OLS_matrix_inversion(X_train, y_train)
+
+        y_test.append(data[test_index])
+        y_pred.append(X_test @ beta)
+
+    # compute expectation value of model prediction
+    y_test = np.array(y_test)
+    y_pred = np.array(y_pred)
+    Exp_y = np.mean(y_pred, axis=0) #, keepdims=True)
+
+    # compute error, bias and variance
+    error =    np.mean( (y_test - y_pred)**2 )
+    bias2 =    np.mean( (y_test - Exp_y)**2 )
+    variance = np.mean( (y_pred - Exp_y)**2 )
+            
+    return error, bias2, variance
+
+
+
+def bias_variance_tradeoff(data, design_arr, resampling_method, method_params=0, max_degree=10, test_size=0.2):
     '''
     Bias-variance tradeoff while looping through model complexity
     '''
@@ -90,6 +155,10 @@ def bias_variance_tradeoff(data, design_arr, n_bootstraps=500, max_degree=10, te
         # defining design matrix
         X = DesignMatrix(design_arr, design_arr, deg)
         
-        error[deg], bias2[deg], var[deg] = error_bias2_variance(X, data, deg, n_bootstraps=n_bootstraps, test_size=test_size)
+        if resampling_method == 'bootstrap':
+            error[deg], bias2[deg], var[deg] = bootstrap(X, data, n_bootstraps=method_params, test_size=test_size)
+        
+        elif resampling_method == "cross-validation":
+            error[deg], bias2[deg], var[deg] = cross_validation(X, data, test_size=test_size)
             
     return error, bias2, var
